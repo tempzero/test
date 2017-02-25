66e5fe2af1c5d5c13fd2954d319c5839cda796fc
tempzero <16360308@qq.com>
2017-02-25 15:18:40 +0800

    Update 001.py

--- 001.py
+++ 001.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 def Hi(x):
     print 'Hello, %s!' %x
 if __name__ == '__main__':
     Hi('Mini Copper')
-=======
-def Hi(x):
-    print 'Hello, %s!' %x
-if __name__ == '__main__':
-    Hi('Mini Copper')
->>>>>>> origin/master
